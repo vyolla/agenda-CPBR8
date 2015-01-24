06fc5d11aaaeb4112e2d37ee5c7997e1d9effc24
Bruno Patrocinio <bruno.patrocinio@embraer.com.br>
2015-01-23 22:23:06 -0200

    Initial Commit

---
+++ Agenda CPBR8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Agenda CPBR8
+//
+//  Created by Bruno Patrocinio on 23/01/15.
+//  Copyright (c) 2015 Bruno Patrocinio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
